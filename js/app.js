7f70b8aac387eab8110d93c450d1c95ff6e5f4d1
kaona <alain.armand@gmail.com>
2015-11-15 19:06:04 -0500

    NickNames LI added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 var ViewModel = function() {
+
+
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Mimi');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.flickr.com')
+	this.imgAttribution = ko.observable('https://www.flickr.com');
 	// adding clickCounter method. the only time we are writing viewModel methods is 
 	// when we have to change something ourselve like with clickCounter.
 	// this code will be a lot smaller. We are not writing octopus code because
@@ -12,16 +14,24 @@ var ViewModel = function() {
 		this.clickCount(this.clickCount() + 1);
 	};
 	this.catLevel = ko.computed(function() {
-		if (this.clickCount() < 11 ) {
+		var clicks = this.clickCount();
+		if (clicks < 11 ) {
 		return 'Newborn';
-		} else if (10 < this.clickCount() < 51 ) {
+		} else if (clicks < 51 ) {
 			return 'Infant';
-		} else  if (50 < this.clickCount() < 100) {
-			this.catLevel  = 'Youngin';
-		} else if (this.clickCount() > 99) {
-		return 'Adult';
+		} else if (clicks < 100) {
+			return 'Youngin';
+		} else {
+			return 'Adult';
 		}
 	}, this);
+
+	this.nickNames = ko.observableArray([
+		{ nickName: 'Mimet'},
+		{ nickName: 'Mrs. Meow'},
+		{ nickName: 'Cat'},
+		{ nickName: 'Mimsy'}
+	]);
 }
 
 ko.applyBindings(new ViewModel());
