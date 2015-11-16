28f879590f038b6d9c2edfb88ee9bd5e028e29f3
kaona <alain.armand@gmail.com>
2015-11-15 19:55:55 -0500

    Updated viewModel, created view

--- js/app.js
+++ js/app.js
@@ -1,29 +1,26 @@
-var ViewModel = function() {
-
+// creating cat function here will allow us to call it from 
+// the viewModel. If we had left this in the view model, we 
+// we would have to access from a for loop each time we created
+// a cat.
+var Cat = function() {
 
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Mimi');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://www.flickr.com');
-	// adding clickCounter method. the only time we are writing viewModel methods is 
-	// when we have to change something ourselve like with clickCounter.
-	// this code will be a lot smaller. We are not writing octopus code because
-	// knockout is handling view -> model, model -> view sync for us. We are no longer
-	// asking the octopus to go get the information from the model for us.
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	};
-	this.catLevel = ko.computed(function() {
+	this.title = ko.computed(function() {
 		var clicks = this.clickCount();
+		var title;
 		if (clicks < 11 ) {
-		return 'Newborn';
+			title = 'Newborn';
 		} else if (clicks < 51 ) {
-			return 'Infant';
+			title = 'Infant';
 		} else if (clicks < 100) {
-			return 'Youngin';
+			title = 'Youngin';
 		} else {
-			return 'Adult';
+			title = 'Adult';
 		}
+		return title;
 	}, this);
 
 	this.nickNames = ko.observableArray([
@@ -33,5 +30,21 @@ var ViewModel = function() {
 		{ nickName: 'Mimsy'}
 	]);
 }
+
+var ViewModel = function() {
+
+	// this is how we create a new cat.
+	// this new cat is a ko observable function.
+	this.currentCat = ko.observable( new Cat() );
+
+	// adding clickCounter method. the only time we are writing viewModel methods is 
+	// when we have to change something ourselve like with clickCounter.
+	// this code will be a lot smaller. We are not writing octopus code because
+	// knockout is handling view -> model, model -> view sync for us. We are no longer
+	// asking the octopus to go get the information from the model for us.
+	this.incrementCounter = function() {
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+	};
+};
 
 ko.applyBindings(new ViewModel());
