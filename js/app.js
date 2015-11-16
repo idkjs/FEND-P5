e14da3a3335760129dfc1f025ee024f3a49477b1
kaona <alain.armand@gmail.com>
2015-11-16 15:06:47 -0500

    added data-bind to view cat area.
    not having this and not seeing it cost me today. Lesson is to re-read
    carefully or to re-write small sections like this when you encounter
    this problem. rewriting is easy if you understand it and you understood
    this.

--- js/app.js
+++ js/app.js
@@ -2,15 +2,62 @@
 // the viewModel. If we had left this in the view model, we 
 // we would have to access from a for loop each time we created
 // a cat.
-var Cat = function() {
 
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Mimi');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.flickr.com');
+// refactoring Cat() so that it can make a new cat every time. Right now it can only
+// use the data we have hard coded into it. Start by creating var data and passing it
+// to Cat()
+/*jshint strict:false */
+
+// replace this with loading from a server or parse.com for extra credit
+var initialCats = [
+	{
+		name: 'Mimi',
+		img: 'img/firstcat.jpg',
+		clickCount: 0,
+		imgAttribution: '',
+		nickNames:['Mimet', 'Mrs. Meow','Cat','Mimsy']
+	},
+	{
+		name: 'Figaro',
+		img: 'img/secondcat.jpg',
+		clickCount: 0,
+		imgAttribution: '',
+		nickNames: ['Figgy']
+	},
+	{
+		name: 'Pharoah',
+		img: 'img/thirdcat.jpg',
+		clickCount: 0,
+		imgAttribution: '',
+		nickNames: ['Faro']
+	},
+	{
+		name: 'Fluffy',
+		img: 'img/fourthcat.jpg',
+		clickCount: 0,
+		imgAttribution: '',
+		nickNames: ['Velu']
+	},
+	{
+		name: 'Kitty',
+		img: 'img/fifthcat.jpg',
+		clickCount: 0,
+		imgAttribution: '',
+		nickNames: ['KitKat']
+	}
+];
+
+var Cat = function(data) {
+
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.img = ko.observable(data.img);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nickNames = ko.observableArray(data.nickNames);
+
 	this.title = ko.computed(function() {
-		var clicks = this.clickCount();
 		var title;
+		var clicks = this.clickCount();
 		if (clicks < 11 ) {
 			title = 'Newborn';
 		} else if (clicks < 51 ) {
@@ -21,30 +68,46 @@ var Cat = function() {
 			title = 'Adult';
 		}
 		return title;
+		console.log(title);
 	}, this);
-
-	this.nickNames = ko.observableArray([
-		{ nickName: 'Mimet'},
-		{ nickName: 'Mrs. Meow'},
-		{ nickName: 'Cat'},
-		{ nickName: 'Mimsy'}
-	]);
-}
+};
 
 var ViewModel = function() {
+	// this var self makes it so that when you use self instead of this, self maps to the 
+	// ViewModel context. when you use self.catlist, you are mapping to the catlist
+	// in the ViewModel
 	var self = this;
 	// this is how we create a new cat.
 	// this new cat is a ko observable function.
-	this.currentCat = ko.observable( new Cat() );
+	// in order to create a new cat, we include the 'data'{}/object into this currentCat.
+	
+	// create ko array for cats
+	this.catList = ko.observableArray([]);
+	// pass the cats from var initialCats into the ko observable array
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem) );
+		console.log(self.catList);
+	});
+
+	this.currentCat = ko.observable( this.catList()[0] );
 
 	// adding clickCounter method. the only time we are writing viewModel methods is 
 	// when we have to change something ourselve like with clickCounter.
 	// this code will be a lot smaller. We are not writing octopus code because
 	// knockout is handling view -> model, model -> view sync for us. We are no longer
 	// asking the octopus to go get the information from the model for us.
+	
+	// docs on getting data from list click http://knockoutjs.com/documentation/click-binding.html
 	this.incrementCounter = function() {
 		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+
+	this.setClickedCat = function(catItem) {
+		self.currentCat(catItem);
 	};
 };
 
 ko.applyBindings(new ViewModel());
+
+
+
